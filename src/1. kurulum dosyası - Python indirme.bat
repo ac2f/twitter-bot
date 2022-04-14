c3c9dfdf62320f746da7de48320b49ad15a6a36e
ac2f <mustafabasarn3@gmail.com>
2022-04-14 13:10:09 +0300

    3

---
+++ src/1. kurulum dosyası - Python indirme.bat
@@ -0,0 +1,7 @@
+echo "Python 3.10.2 kurulum dosyası 'python-3.10.4-amd64.exe' dosyasına kaydedilecek. Devam etmek için <ENTER> tuşuna basın.."
+pause
+curl https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe --output python-3.10.4-amd64.exe
+echo "Dosya indirildi! Lütfen kurulumu tamamlayın."
+start python-3.10.4-amd64.exe
+echo "KURULUMU TAMAMLADIYSANIZ DEVAM ETMEK İÇİN <ENTER> TUŞUNA BASIN!"
+pause
